eba0b52ff19f3bd941f48cff059923444f94e374
李毅杰 <3286235592@qq.com>
2026-03-27 23:09:54 +0800

    111

--- src/i18n/languages/en.ts
+++ src/i18n/languages/en.ts
@@ -5,6 +5,7 @@ export const en: Translation = {
     [Key.home]: "Home",
     [Key.about]: "About",
     [Key.archive]: "Archive",
+    [Key.link]: "Link",
     [Key.search]: "Search",
 
     [Key.tags]: "Tags",
