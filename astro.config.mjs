2c903d294cf70071c047c0856436e0640f5a32ce
李毅杰 <3286235592@qq.com>
2026-08-13 22:31:11 +0800

    Update production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dear-nikki.vercel.app/",
+    site: "https://seadream.vercel.app/",
     base: "/",
     trailingSlash: "always",
     integrations: [
